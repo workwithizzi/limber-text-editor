3f8e2f1df9187d10be34a517b40e4df0a7b9143e
IvaTsu <ivan.tsud@gmail.com>
2019-12-13 23:07:26 +0200

    Refactor 'text-align' format

---
+++ components/slate-editor-formats/text-align/index.js
@@ -0,0 +1,10 @@
+// Handle TextAlign components export
+
+import hasMultipleAligns from "./hasMultipleAligns";
+import { renderAlignButton, renderAlignButtons } from "./renderToolbarButtons";
+
+export {
+	renderAlignButton,
+	renderAlignButtons,
+	hasMultipleAligns,
+};
